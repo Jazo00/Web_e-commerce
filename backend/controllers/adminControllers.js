962b07b8e80875325ac7011ed682ff95239f9966
realjordanfaciol <realjordanfaciol@gmail.com>
2024-10-29 22:27:41 +0800

    Finished setting up the initial controller for admin

--- backend/controllers/adminControllers.js
+++ backend/controllers/adminControllers.js
@@ -10,10 +10,62 @@ const getAllAdmins = async (req, res) => {
   res.status(200).json(admins);
 };
 
-const createAdmin = async (req, res) => {};
+const createAdmin = async (req, res) => {
+  const { userId, userManagement, reports, tradeMonitoring } = req.body;
 
-const updateAdmin = async (req, res) => {};
+  if (![userId, userManagement, reports, tradeMonitoring].every(Boolean)) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
 
-const deleteAdmin = async (req, res) => {};
+  const adminObject = {
+    userId,
+    userManagement,
+    reports,
+    tradeMonitoring,
+  };
+
+  await Admin.create(adminObject);
+
+  res.status(201).json({ message: `New admin created` });
+};
+
+const updateAdmin = async (req, res) => {
+  const { id, userId, userManagement, reports, tradeMonitoring } = req.body;
+
+  if (![id, userId, userManagement, reports, tradeMonitoring].every(Boolean)) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
+
+  const foundAdmin = await Admin.findById(id).exec();
+
+  if (!foundAdmin) {
+    return res.status(400).json({ message: 'Admin not found' });
+  }
+
+  foundAdmin.userId = userId;
+  foundAdmin.userManagement = userManagement;
+  foundAdmin.reports = reports;
+  foundAdmin.tradeMonitoring = tradeMonitoring;
+
+  res.status(201).json({ message: `Admin updated` });
+};
+
+const deleteAdmin = async (req, res) => {
+  const { id } = req.body;
+
+  if (!id) {
+    return res.status(400).json({ message: 'Admin ID is required' });
+  }
+
+  const foundAdmin = await Admin.findById(id).exec();
+
+  if (!foundAdmin) {
+    return res.status(400).json({ message: 'Admin not found' });
+  }
+
+  await foundAdmin.deleteOne();
+
+  res.status(200).json({ message: 'Admin deleted' });
+};
 
 module.exports = { getAllAdmins, createAdmin, updateAdmin, deleteAdmin };
